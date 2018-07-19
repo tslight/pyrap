2e25cf5f2a348e7205379660cac33f0997308cf2
tslight <tobyslight@gmail.com>
2018-07-19 21:07:04 +0100

    almost there...

--- pyrap/pyrap.py
+++ pyrap/pyrap.py
@@ -24,7 +24,7 @@ def get_last(path):
         pipe = "awk '{print $5}' | sed '/\./d' | sort -nr | awk 'NR==2'"
         return subprocess.Popen(rsync + pipe,
                                 shell=True, stdout=subprocess.PIPE,
-                                universal_newlines=True).stdout.read()
+                                universal_newlines=True).stdout.read().strip()
     else:
         return sorted(os.listdir(path), reverse=True)[0]
 
@@ -36,16 +36,19 @@ def get_excludes(excludes, path, hidden):
     """
     # merge excludes, list + set removes duplicates.
     excludes = list(set(excludes + curses.wrapper(pick, path, hidden)))
-    if len(excludes) > 0:
-        print("\nSelected excludes:\n"+('\n'.join(excludes)))
-        if ask("\nAccept and use excludes? "):
-            return excludes
-        else:
-            excludes = list(set(excludes + curses.wrapper(pick, path, hidden)))
+    print("\nSelected excludes:\n"+('\n'.join(excludes)))
+    if ask("\nAccept and use excludes? "):
+        return excludes
     else:
-        if not(ask("\nNo excludes selected. Continue? ")):
-            excludes = list(set(excludes + curses.wrapper(pick, path, hidden)))
+        excludes = list(set(excludes + curses.wrapper(pick, path, hidden)))
     return excludes
+
+
+def mkxargs(excludes):
+    xargs = []
+    for x in excludes:
+        xargs.append('--exclude="' + x + '"')
+    return xargs
 
 
 def copy_skel(opts, date, user, url):
@@ -57,6 +60,18 @@ def copy_skel(opts, date, user, url):
 
 
 def pyrap(opts, src, dest, automate_excludes):
+    cmd = "rsync" + " " + rargs + " " + src + " " + dest
+    subprocess.call(cmd, shell=True)
+
+
+def process(args, users):
+    hidden = True
+
+    opts = [
+        '--archive ',
+        '--human-readable ',
+    ]
+
     excludes = [
         '*.ost',
         '*.pst',
@@ -65,20 +80,6 @@ def pyrap(opts, src, dest, automate_excludes):
         'desktop.ini',
         '*spotify*',
         '*Spotify*',
-    ]
-
-    hidden = True
-    if not automate_excludes:
-        excludes = get_excludes(excludes, src, hidden)
-    rargs = "rsync " + " ".join(opts) + " ".join(excludes)
-    cmd = rargs + " " + src + " " + dest
-    subprocess.call(cmd, shell=True)
-
-
-def process(args, users):
-    opts = [
-        '--archive ',
-        '--human-readable ',
     ]
 
     if len(users) > 0:
@@ -90,8 +91,8 @@ def process(args, users):
                 src = home + "/"
                 dest = args.url + "/Users/" + user + "/" + date
                 copy_skel(opts, date, user, args.url)
-                opts.append('--link-dest=' +
-                            get_last(args.url + "/Users/" + user) + ' ')
+                lastbkup = get_last(args.url + "/Users/" + user)
+                opts.append('--link-dest="../' + lastbkup + '" ')
             elif args.restore:
                 copytype = "restore"
                 upath = (args.url + "/Users/" + user)
@@ -100,10 +101,18 @@ def process(args, users):
                 dest = home
 
             if args.users:
-                pyrap(opts, src, dest, args.excludes)
+                if not args.excludes:
+                    excludes = get_excludes(excludes, src, hidden)
+                rargs = " ".join(opts) + " ".join(mkargs(excludes))
+                pyrap(rargs, src, dest)
             else:
                 question = "\n" + copytype.title() + " " + user + "? "
                 if ask(question):
-                    pyrap(opts, src, dest, args.excludes)
+                    if not args.excludes:
+                        excludes = get_excludes(excludes, src, hidden)
+                    rargs = " ".join(opts) + " ".join(mkxargs(excludes))
+                    import pdb
+                    pdb.set_trace()
+                    pyrap(rargs, src, dest)
     else:
         print("No users to %s" % copytype)
